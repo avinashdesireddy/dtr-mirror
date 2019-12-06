c16cc7113092e913d93fcc2d8d88c87fc9ba5a74
Avinash Desireddy <avinashr.desireddy@gmail.com>
2019-12-06 15:06:54 +0000

    Get Tags list

--- dtr_repo_info.sh
+++ dtr_repo_info.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+function status (){
+  echo " Usage: $0 <repo-file-name>"
+  exit 1
+}
+
+if [ -z "$1" ]
+  then
+    status
+fi
 ## Capture DTR Info
 [ -z "$DTR_HOSTNAME" ] && read -p "Enter the DTR hostname and press [ENTER]:" DTR_HOSTNAME
 [ -z "$DTR_USER" ] && read -p "Enter the DTR username and press [ENTER]:" DTR_USER
 [ -z "$DTR_PASSWORD" ] && read -s -p "Enter the DTR token or password and press [ENTER]:" DTR_PASSWORD
 echo "***************************************\\n"
 
-RESULTS_FILE=repositories.json
+RESULTS_FILE=$1
+: > $RESULTS_FILE
 
 ## Extract repositories info
 repos=$(curl -ks -u ${DTR_USER}:${DTR_PASSWORD} -X GET "https://${DTR_HOSTNAME}/api/v0/repositories?pageSize=100000&count=true" -H "accept: application/json" | jq -r -c .repositories)
---
+++ get_tags.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+function status (){
+  echo " Usage: $0 <repo-file-name> <tags-filename>"
+  exit 1
+}
+
+if [ -z "$2" ]
+  then
+    status
+fi
+
+## Capture DTR Info
+[ -z "$DTR_HOSTNAME" ] && read -p "Enter the DTR hostname and press [ENTER]:" DTR_HOSTNAME
+[ -z "$DTR_USER" ] && read -p "Enter the DTR username and press [ENTER]:" DTR_USER
+[ -z "$DTR_PASSWORD" ] && read -s -p "Enter the DTR token or password and press [ENTER]:" DTR_PASSWORD
+echo "***************************************\\n"
+
+REPOSITORIES_FILE=$1
+TAGS_FILE=$2
+: > $2
+
+TOKEN=$(curl -kLsS -u ${DTR_USER}:${DTR_PASSWORD} "https://${DTR_HOSTNAME}/auth/token" | jq -r '.token')
+CURLOPTS=(-kLsS -H 'accept: application/json' -H 'content-type: application/json' -H "Authorization: Bearer ${TOKEN}")
+
+## Read repositories file
+repo_list=$(cat ${REPOSITORIES_FILE} | jq -c -r '.[]') 
+
+# Loop through repositories
+while IFS= read -r row ; do
+    namespace=$(echo "$row" | jq -r .namespace)
+    reponame=$(echo "$row" | jq -r .name)
+
+    tags=$(curl -ksLS -u ${DTR_USER}:${DTR_PASSWORD} -X GET "https://$DTR_HOSTNAME/api/v0/repositories/${namespace}/${reponame}/tags?pageSize=100000000")
+    tags_list=$(echo $tags | jq -c -r '.[]')
+    if [ -z "$tags_list" ]
+        then
+            continue
+    fi
+    echo "Repository ==> Org: ${namespace}/${reponame}"
+    while IFS= read -r tag ; do
+        tagname=$(echo "$tag" | jq -r .name)
+        echo ${DTR_HOSTNAME}/${reponame}:${tagname} >> $TAGS_FILE
+    done <<< "$tags_list"
+    
+done <<< "$repo_list"
+echo "=========================================\\n"
+
